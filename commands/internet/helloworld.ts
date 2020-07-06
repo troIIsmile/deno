4ad9e8d4e7129fda43d81b614d95bb3f16a0300e
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 13:01:18 -0400

    add helloworld

---
+++ commands/internet/helloworld.ts
@@ -0,0 +1,57 @@
+import { Message, Options } from "https://deno.land/x/coward@dev/mod.ts";
+import { Bot } from '../../utils/types.ts'
+import random from "../../utils/random.ts";
+declare module API {
+  export interface Payload {
+    ".code.tio": string;
+  }
+
+  export interface Request {
+    command: string;
+    payload: Payload;
+  }
+
+  export interface HelloWorld {
+    request: Request[];
+    response: string;
+  }
+
+  export interface Tests {
+    helloWorld: HelloWorld;
+  }
+
+  export interface Language {
+    categories: string[];
+    encoding: string;
+    link: string;
+    name: string;
+    tests: Tests;
+    update: string;
+  }
+
+  export interface List {
+    [key: string]: Language;
+  }
+}
+
+export async function run(): Promise<Options.createMessage> {
+  const langs: API.List = await fetch("https://tio.run/languages.json").then(
+    (res) => res.json()
+  );
+  const { name: title, tests, link: url } = random(Object.entries(langs))[1];
+  return {
+    embed: {
+      title,
+      url,
+      description: `\`\`\`
+${random(tests.helloWorld.request).payload[".code.tio"]}
+\`\`\``,
+      fields: [{
+        name: "Prints",
+        value: tests.helloWorld.response,
+      }],
+    },
+  };
+}
+export const help = "Hello world in a random programming language";
+export const aliases = [];
