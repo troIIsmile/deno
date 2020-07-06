42ead164bb3d97133eac9317e8650019b0638b40
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 17:01:07 -0400

    add dl alias

--- commands/internet/download.ts
+++ commands/internet/download.ts
@@ -1,29 +1,30 @@
-import { Message, Options } from "https://deno.land/x/coward@dev/mod.ts";
-import { Bot } from "../../utils/types.ts";
-export async function run(
+import { Message, Options } from "https://deno.land/x/coward@dev/mod.ts"
+import { Bot } from "../../utils/types.ts"
+export async function run (
   this: Bot,
   _message: Message,
   args: string[],
 ): Promise<Options.createMessage> {
   const res = await fetch(
     `https://projectlounge.pw/ytdl/download?url=${
-      encodeURIComponent(
-        args.join(" "),
-      )
+    encodeURIComponent(
+      args.join(" "),
+    )
     }`,
-  );
+  )
   if (res.ok) {
     return {
       file: {
         file: await res.blob(),
         name: "video.mp4",
       },
-    };
+    }
   } else {
     return {
       content: "The download servers had an issue: " +
         `${res.status}: ${res.statusText}`,
-    };
+    }
   }
 }
-export const help = "Download a video.";
+export const help = "Download a video."
+export const aliases = ['dl']
