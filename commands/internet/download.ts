217cb3f0bfeabfdca316b9bfb1f299ff437de9e1
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 12:47:52 -0400

    formatting fuck deno style personally

--- commands/internet/download.ts
+++ commands/internet/download.ts
@@ -1,23 +1,30 @@
 import { Message, Options } from "https://deno.land/x/coward@dev/mod.ts";
-import { Bot } from '../../utils/types.ts'
-export async function run (this: Bot, message: Message, args: string[]): Promise<Options.createMessage> {
+import { Bot } from "../../utils/types.ts";
+export async function run(
+  this: Bot,
+  _message: Message,
+  args: string[],
+): Promise<Options.createMessage> {
   const res = await fetch(
-  `https://projectlounge.pw/ytdl/download?url=${
-    encodeURIComponent(
-      args.join(" "),
-    )
-  }`,
-  )
+    `https://projectlounge.pw/ytdl/download?url=${
+      encodeURIComponent(
+        args.join(" "),
+      )
+    }`,
+  );
   if (res.ok) {
     return {
       file: {
         file: await res.blob(),
-        name: 'video.mp4'
-      }
-    }
-  } else return {
-    content: 'The download servers had an issue: ' + `${res.status}: ${res.statusText}`
+        name: "video.mp4",
+      },
+    };
+  } else {
+    return {
+      content: "The download servers had an issue: " +
+        `${res.status}: ${res.statusText}`,
+    };
   }
 }
-export const help = 'Download a video.'
-export const aliases = []
+export const help = "Download a video.";
+export const aliases = [];
