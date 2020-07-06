16c0f1e8c48ff2d705c768e99a5e102321cccb84
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 02:21:25 -0400

    trim hello world

--- commands/string/votepoop.ts
+++ commands/string/votepoop.ts
@@ -1,8 +1,3 @@
-import { Options } from "https://deno.land/x/coward@dev/mod.ts";
-export async function run(): Promise<Options.createMessage> {
-  return {
-    content: "😎 i voted for poop",
-  };
-}
+export const run = () =>  "😎 i voted for poop"
 export const help = "Hello, world!";
 export const aliases = [];
