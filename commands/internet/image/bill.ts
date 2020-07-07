966114fbff6e1d9cb9e80b68c780a561f490693b
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 23:15:56 -0400

    i am stupid

--- commands/internet/image/bill.ts
+++ commands/internet/image/bill.ts
@@ -1,17 +1,16 @@
 import { Options, Message } from "https://deno.land/x/coward@dev/mod.ts";
-import { DOMParser } from "https://raw.githubusercontent.com/rtbenfield/deno-dom/master/mod.ts";
 import { stringify } from "https://deno.land/std/node/querystring.ts";
 export async function run(message: Message): Promise<Options.createMessage> {
   const male = Number(
-    new DOMParser().parseFromString(
-      await fetch(
-        "https://pbump.net/files/post/names/find.php?who=" +
-          encodeURIComponent(message.author.username),
-      ).then(
-        (res) => res.text(),
-      ),
-      "text/xml",
-    ).getElementsByTagName("like").item(0)?.getAttribute("male"),
+    (await fetch(
+      "https://pbump.net/files/post/names/find.php?who=" +
+        encodeURIComponent(message.author.username),
+    ).then(
+      (res) => res.text(),
+    )).split("\n")[1].replace(
+      /[^0-9\.]+/g,
+      "",
+    ),
   );
 
   return {
