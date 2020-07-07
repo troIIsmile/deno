8b11f36c4908982adf3ae161e33f8a3437b99089
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 23:10:39 -0400

    slightly more readable code in bill

--- commands/internet/image/bill.ts
+++ commands/internet/image/bill.ts
@@ -1,5 +1,6 @@
 import { Options, Message } from "https://deno.land/x/coward@dev/mod.ts";
 import { DOMParser } from "https://raw.githubusercontent.com/rtbenfield/deno-dom/master/mod.ts";
+import { stringify } from "https://deno.land/std/node/querystring.ts";
 export async function run(message: Message): Promise<Options.createMessage> {
   const male = Number(
     new DOMParser().parseFromString(
@@ -13,11 +14,14 @@ export async function run(message: Message): Promise<Options.createMessage> {
     ).getElementsByTagName("like").item(0)?.getAttribute("male"),
   );
 
-  const sex = male >= 0.5 || male === -1 ? "m" : "f";
   return {
     file: {
       file: await fetch(
-        `https://belikebill.ga/billgen-API.php?default=1&name=${encodeURIComponent(message.author.username)}&sex=` + sex,
+        "https://belikebill.ga/billgen-API.php?" + stringify({
+          name: message.author.username,
+          default: 1,
+          sex: male >= 0.5 || male === -1 ? "m" : "f",
+        }),
       ).then((res) => res.blob()),
       name: "bill.jpeg",
     },
