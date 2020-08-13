66d51b928b565f01ad68bb977c987a26d1eae8e2
Jack <29169102+Jack5079@users.noreply.github.com>
2020-08-13 14:34:58 -0400

    fix import map

--- bot.ts
+++ bot.ts
@@ -1,16 +1,16 @@
 import { Client } from "coward";
 import { Bot } from "./utils/types.ts";
 import { token } from "./config.ts";
-import messageCreate from './events/messageCreate.ts'
+import messageCreate from "./events/messageCreate.ts";
 import ready from "./events/ready.ts";
-const client: Bot = <Bot>new Client(token);
+const client: Bot = <Bot> new Client(token);
 client.commands = new Map();
 client.aliases = new Map();
 
 // Load events
-client.evt.messageCreate.attach(ctx => {
-  messageCreate.bind(client, ctx.message)
-})
+client.evt.messageCreate.attach((ctx) => {
+  messageCreate.bind(client, ctx.message);
+});
 
 client.evt.ready.attach(() => {
   ready.bind(client);
